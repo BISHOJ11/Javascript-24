fa85adf7a76ef28292a3c3397cbb18bfb9d5b7f2
BISHOJ11 <40242266+BISHOJ11@users.noreply.github.com>
2024-03-09 15:46:53 +0000

    Variables in Javascript

---
+++ 01_basics/01_var.js
@@ -0,0 +1,10 @@
+ const birthYear = 1996
+ let job = "office "
+ var locaton ="patandhoka"
+ 
+ console.table([birthYear, job, locaton]);
+ 
+ /*
+ const => Determines its value is Constant (It's  situation that its value does not change.)
+  Prefer to not to use var because it lacks and has isssue in block scope and functional scope 
+ */
