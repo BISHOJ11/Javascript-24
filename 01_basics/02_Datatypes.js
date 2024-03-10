714a3ada17f55f8eb3da30167654907a7b33a9b6
BISHOJ11 <40242266+BISHOJ11@users.noreply.github.com>
2024-03-10 15:49:04 +0000

    Datatypes in Javascript

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+let rollNo = 10
+let subject = "Science"
+let product;
+ console.table([typeof rollNo, subject, product])
+
+
+
+
+
+
+
+
+/*
+Datatypes in javascript
+
+* Number DataType
+* String DataType
+* Boolean Datatype
+* Null Datatype
+* Undefined Datatype
+
+null dt is type value its not 0 value but doesnt want any kind of value in that condition we use null Dt 
+
+undefined Dt  is value where value is not defined by user.
+
+
+*/
