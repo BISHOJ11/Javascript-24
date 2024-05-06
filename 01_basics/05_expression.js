db79ed2c21e93d6fbeae64647874956da64696c3
BISHOJ11 <40242266+BISHOJ11@users.noreply.github.com>
2024-05-06 16:29:24 +0000

    Expression in Javascript

---
+++ 01_basics/05_expression.js
@@ -0,0 +1,15 @@
+/*
+first type of expression
+
+[x=7]
+expression  uses the = operator to assign the value seven to variable
+
+
+second type of expression
+[4+3]
+expression uses the  + operator to add  4and3
+lets see example Expression
+
+*/
+const z = 19 + 5 ;
+console.log(z);
