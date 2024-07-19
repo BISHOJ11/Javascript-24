3d0e80e1b52f502f4fdd3e5724948c10091d957a
BISHOJ11 <40242266+BISHOJ11@users.noreply.github.com>
2024-07-19 07:07:27 +0000

    Array.is.Array Prototype explain with detail example and conditions and  Array.from and Array.of

--- 01_basics/13_is_array.js
+++ 01_basics/13_is_array.js
@@ -49,3 +49,28 @@ console.log(Array.isArray(false));
 console.log(Array.isArray(new Uint8Array(32)));
 console.log(Array.isArray("a","e","i","o","u"));
 
+
+
+/*
+Array.from
+It genrally converts  other datatypes into array
+*/
+
+console.log(Array.from("Github"));
+//===> [G],[i],[t],[h],[u],[b]
+
+console.log(Array.from({repo: "javascript"}));
+//===> []
+
+
+    /*
+    Array.of
+    It returns a new array from set of elements
+    */
+
+    const dell = 25000
+    const poco = 16000
+    const redmi = 10000
+
+    console.log(Array.of(dell,poco,redmi));
+    //==> [25000,16000,10000]
